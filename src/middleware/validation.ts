6d3e3d00a18b921e4f84378dbc7102cdd6847491
brucechanch <brucechanch@gmail.com>
2024-04-26 12:09:35 +0800

    create restaurant API

--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -20,3 +20,16 @@ export const validateMyUserRequest = [
   body("country").isString().notEmpty().withMessage("Country must be a string"),
   handleValidationErrors,
 ]
+
+export const validateMyRestaurantRequest = [
+body('restaurantName').notEmpty().withMessage("Restaurant is required"),
+body('city').notEmpty().withMessage("Restaurant is required"),
+body('country').notEmpty().withMessage("Restaurant is required"),
+body('deliveryPrice').isFloat({min: 0}).withMessage("Delivery price must be a positive number"),
+body("estimatedDeliveryTime").isInt({min: 0}).withMessage("Estimated delivery time must be a positive integer"),
+body("cuisines").isArray().withMessage("Cuisines must be an array").not().isEmpty().withMessage("Cuisines must not be empty"),
+body("menuItems").isArray().withMessage("Menu items must be an array"),
+body("menuItems.*.name").notEmpty().withMessage("Menu item name is required"),
+body("menuItems.*.price").isFloat({min:0}).withMessage("Menu item price is required and must be a positive number"),
+handleValidationErrors,
+]
