f2f6187356d07dddf7f904f179ca0c4d8171e747
brucechanch <brucechanch@gmail.com>
2024-04-29 15:07:51 +0800

    search api done

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import mongoose from "mongoose"
 import myUserRoute from "./routes/MyUserRoute"
 import { v2 as cloudinary } from "cloudinary"
 import myRestaurantRoute from "./routes/MyRestaurantRoute"
+import restaurantRoute from "./routes/RestaurantRoute"
 
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => console.log("Connected to Database!"))
@@ -28,7 +29,7 @@ app.get("/health", async(req: Request, res: Response) => {
 
 app.use("/api/my/user", myUserRoute);
 app.use("/api/my/restaurant", myRestaurantRoute);
-
+app.use("/api/restaurant", restaurantRoute)
 
 const port = 7001
 app.listen(port, () => {
