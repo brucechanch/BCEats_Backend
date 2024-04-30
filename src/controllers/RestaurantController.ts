e8c32b0b49f7354c2f3a417e597c264388a3fd3b
brucechanch <brucechanch@gmail.com>
2024-04-30 16:18:15 +0800

    Detail Page and Cart function

--- src/controllers/RestaurantController.ts
+++ src/controllers/RestaurantController.ts
@@ -1,6 +1,21 @@
 import { Request, Response } from 'express';
 import Restaurant from '../models/restaurant';
 
+
+const getRestaurant = async(req: Request, res: Response) => {
+  try {
+    const restaurantId = req.params.restaurantId;
+
+    const restaurant = await Restaurant.findById(restaurantId)
+    if(!restaurant){
+      return res.status(404).json({message: "Restaurant not found"})
+    }
+res.json(restaurant)
+  } catch (error) {
+  console.log(error);
+  res.status(500).json({message: "Something went wrong"})
+  }
+}
 const searchRestaurant = async(req: Request, res:Response) => {
 try {
   const city = req.params.city;
@@ -67,4 +82,5 @@ res.json(response);
 
 export default {
   searchRestaurant,
+  getRestaurant,
 }
--- src/models/restaurant.ts
+++ src/models/restaurant.ts
@@ -1,10 +1,15 @@
 import mongoose from "mongoose";
 
 const menuItemSchema = new mongoose.Schema({
-  name: {type: String, required: true},
-  price: {type: Number, required: true},
+  _id: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    default: () => new mongoose.Types.ObjectId(),
+  },
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+});
 
-})
 
 const restaurantSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref:"User"},
