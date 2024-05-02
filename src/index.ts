4a1fa51d002293e6d243a32479b5e8b161f9af7f
brucechanch <brucechanch@gmail.com>
2024-05-02 15:41:45 +0800

    adding checkout flow

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import myUserRoute from "./routes/MyUserRoute"
 import { v2 as cloudinary } from "cloudinary"
 import myRestaurantRoute from "./routes/MyRestaurantRoute"
 import restaurantRoute from "./routes/RestaurantRoute"
+import orderRoute from './routes/OrderRoute'
+
 
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => console.log("Connected to Database!"))
@@ -18,9 +20,12 @@ cloudinary.config({
 });
 
 const app = express()
-app.use(express.json())
+
 app.use(cors())
 
+app.use("/api/order/checkout/webhook", express.raw({ type: "*/*" }));
+
+app.use(express.json())
 
 app.get("/health", async(req: Request, res: Response) => {
 
@@ -30,6 +35,7 @@ app.get("/health", async(req: Request, res: Response) => {
 app.use("/api/my/user", myUserRoute);
 app.use("/api/my/restaurant", myRestaurantRoute);
 app.use("/api/restaurant", restaurantRoute)
+app.use("/api/order", orderRoute)
 
 const port = 7001
 app.listen(port, () => {
